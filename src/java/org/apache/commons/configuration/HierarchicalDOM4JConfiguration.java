662cd0338d6063d7974637c75c77cb5adf4164f9
No Author <dev-null@apache.org>
2004-08-14 12:00:40 +0000

    This commit was manufactured by cvs2svn to create tag 'CONFIGURATION_1_0_RC1'.
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/commons/proper/configuration/tags/CONFIGURATION_1_0_RC1@141922 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/java/org/apache/commons/configuration/HierarchicalDOM4JConfiguration.java
@@ -0,0 +1,173 @@
+package org.apache.commons.configuration;
+
+/*
+ * Copyright 2004 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Iterator;
+
+import org.dom4j.Attribute;
+import org.dom4j.Document;
+import org.dom4j.DocumentException;
+import org.dom4j.Element;
+import org.dom4j.io.SAXReader;
+
+/**
+ * <p>A specialized hierarchical configuration class that is able to parse
+ * XML documents using DOM4J.</p>
+ * <p>The parsed document will be stored keeping its structure. The
+ * contained properties can be accessed using all methods supported by
+ * the base class <code>HierarchicalProperties</code>.
+ * 
+ * @version $Id: HierarchicalDOM4JConfiguration.java,v 1.4 2004/02/27 17:41:35 epugh Exp $
+ */
+public class HierarchicalDOM4JConfiguration
+    extends HierarchicalConfiguration
+    implements BasePathLoader
+{
+    /** Stores the file name of the document to be parsed.*/
+    private String file;
+
+    /** Stores the base path of this configuration.*/
+    private String basePath;
+
+    /**
+     * Creates a new instance of <code>HierarchicalDOM4JConfiguration</code>.
+     */
+    public HierarchicalDOM4JConfiguration()
+    {
+        super();
+    }
+
+    /**
+     * Returns the name of the file to be parsed by this object.
+     * @return the file to be parsed
+     */
+    public String getFileName()
+    {
+        return file;
+    }
+
+    /**
+     * Sets the name of the file to be parsed by this object.
+     * @param file the file to be parsed
+     */
+    public void setFileName(String file)
+    {
+        this.file = file;
+    }
+
+    /**
+     * Returns the base path.
+     * @return the base path
+     */
+    public String getBasePath()
+    {
+        return basePath;
+    }
+
+    /**
+     * Allows to set a base path. Relative file names are resolved based on
+     * this path.
+     * @param path the base path; this can be a URL or a file path
+     */
+    public void setBasePath(String path)
+    {
+        basePath = path;
+    }
+
+    /**
+     * Loads and parses an XML document. The file to be loaded must have
+     * been specified before.
+     * @throws Exception if an error occurs
+     */
+    public void load() throws ConfigurationException
+    {
+    	try {
+    		load(ConfigurationUtils.getURL(getBasePath(), getFileName()));
+    	}
+    	catch (MalformedURLException mue){
+    		throw new ConfigurationException("Could not load from " + getBasePath() + ", " + getFileName());
+    	}
+    }
+
+    /**
+     * Loads and parses the specified XML document.
+     * @param url the URL to the XML document
+     * @throws Exception if an error occurs
+     */
+    public void load(URL url) throws ConfigurationException
+    {
+    	try {
+    		initProperties(new SAXReader().read(url));
+    	}
+    	catch (DocumentException de){
+    		throw new ConfigurationException("Could not load from " + url);
+    	}
+    }
+
+    /**
+     * Initializes this configuration from an XML document.
+     * @param document the document to be parsed
+     */
+    public void initProperties(Document document)
+    {
+        constructHierarchy(getRoot(), document.getRootElement());
+    }
+
+    /**
+     * Helper method for building the internal storage hierarchy. The XML
+     * elements are transformed into node objects.
+     * @param node the actual node
+     * @param element the actual XML element
+     */
+    private void constructHierarchy(Node node, Element element)
+    {
+        if (element.getTextTrim().length() > 0)
+        {
+            node.setValue(element.getTextTrim());
+        } /* if */
+        processAttributes(node, element);
+
+        for (Iterator it = element.elementIterator(); it.hasNext();)
+        {
+            Element child = (Element) it.next();
+            Node childNode = new Node(child.getName());
+            constructHierarchy(childNode, child);
+            node.addChild(childNode);
+        } /* for */
+    }
+
+    /**
+     * Helper method for constructing node objects for the attributes of the
+     * given XML element.
+     * @param node the actual node
+     * @param element the actual XML element
+     */
+    private void processAttributes(Node node, Element element)
+    {
+        for (Iterator it = element.attributeIterator(); it.hasNext();)
+        {
+            Attribute attr = (Attribute) it.next();
+            Node child =
+                new Node(
+                    ConfigurationKey.constructAttributeKey(attr.getName()));
+            child.setValue(attr.getValue());
+            node.addChild(child);
+        } /* for */
+    }
+}
---
+++ src/test/org/apache/commons/configuration/MockConfiguration.java
@@ -0,0 +1,23 @@
+/*
+ * Created on Feb 14, 2004
+ *
+ * To change the template for this generated file go to
+ * Window - Preferences - Java - Code Generation - Code and Comments
+ */
+package org.apache.commons.configuration;
+
+
+/**
+ * @author Eric Pugh
+ *
+ * To change the template for this generated type comment go to
+ * Window - Preferences - Java - Code Generation - Code and Comments
+ */
+public class MockConfiguration extends BaseConfiguration
+{
+    public MockConfiguration(){
+        super();
+    }
+    
+
+}
