9f8d1c1fc7e227b4f65fa302274b5d6e9c255af0
Oliver Heger <oheger@apache.org>
2008-12-14 21:28:34 +0000

    Added missing ASF license header to some files.
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/configuration/trunk@726534 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/org/apache/commons/configuration/TestMultiFileHierarchicalConfiguration.java
+++ src/test/org/apache/commons/configuration/TestMultiFileHierarchicalConfiguration.java
@@ -1,3 +1,19 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.commons.configuration;
 
 import junit.framework.Test;
@@ -13,7 +29,7 @@ public class TestMultiFileHierarchicalConfiguration
     extends TestCase
 {
     private static String PATTERN1 = "target/test-classes/testMultiConfiguration_${sys:Id}.xml";
-    
+
     /**
      * Create the test case
      *
@@ -40,7 +56,7 @@ public class TestMultiFileHierarchicalConfiguration
         //set up a reloading strategy
         FileChangedReloadingStrategy strategy = new FileChangedReloadingStrategy();
         strategy.setRefreshDelay(10000);
-        
+
         MultiFileHierarchicalConfiguration config = new MultiFileHierarchicalConfiguration(PATTERN1);
         config.setReloadingStrategy(strategy);
 
