5af67a13075fd8976c16ed3032ff6436326a0db2
Ralph Goers <rgoers@apache.org>
2009-04-23 23:32:24 +0000

    Allow catalog resolver to use FileSystem. Have System Properties configuration use DefaultConfigurationBuilder's base path
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/configuration/branches/configuration2_experimental@768096 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/commons/configuration2/resolver/CatalogResolver.java
+++ src/main/java/org/apache/commons/configuration2/resolver/CatalogResolver.java
@@ -19,13 +19,20 @@ package org.apache.commons.configuration2.resolver;
 import org.xml.sax.EntityResolver;
 import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
-import org.apache.xml.resolver.CatalogManager;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.impl.NoOpLog;
 import org.apache.commons.configuration2.FileSystem;
+import org.apache.commons.configuration2.ConfigurationException;
+import org.apache.commons.configuration2.ConfigurationUtils;
+import org.apache.xml.resolver.readers.CatalogReader;
+import org.apache.xml.resolver.CatalogException;
 
+import java.net.FileNameMap;
+import java.net.URLConnection;
 import java.net.URL;
 import java.io.InputStream;
+import java.io.IOException;
+import java.util.Vector;
 
 /**
  * Thin wrapper around xml commons CatalogResolver to allow list of catalogs
@@ -44,24 +51,29 @@ public class CatalogResolver implements EntityResolver
     private static final int DEBUG_ALL = 9;
 
     /**
+     * Normal debug setting.
+     */
+    private static final int DEBUG_NORMAL = 4;
+
+    /**
      * Debug nothing.
      */
     private static final int DEBUG_NONE = 0;
-
-    /**
-     * The CatalogResolver
-     */
-    protected org.apache.xml.resolver.tools.CatalogResolver resolver;
 
     /**
      * The CatalogManager
      */
     protected CatalogManager manager = new CatalogManager();
 
-     /**
+    /**
      * The FileSystem in use.
      */
     protected FileSystem fs = FileSystem.getDefaultFileSystem();
+
+    /**
+     * The CatalogResolver
+     */
+    private org.apache.xml.resolver.tools.CatalogResolver resolver;
 
     /**
      * Stores the logger.
@@ -75,6 +87,8 @@ public class CatalogResolver implements EntityResolver
     {
         manager.setIgnoreMissingProperties(true);
         manager.setUseStaticCatalog(false);
+        manager.setCatalogClassName(Catalog.class.getName());
+        manager.setFileSystem(fs);
         setLogger(null);
     }
 
@@ -86,7 +100,6 @@ public class CatalogResolver implements EntityResolver
     public void setCatalogFiles(String catalogs)
     {
         manager.setCatalogFiles(catalogs);
-        resolver = new org.apache.xml.resolver.tools.CatalogResolver(manager);
     }
 
     /**
@@ -96,6 +109,16 @@ public class CatalogResolver implements EntityResolver
     public void setFileSystem(FileSystem fileSystem)
     {
         this.fs = fileSystem;
+        manager.setFileSystem(fileSystem);
+    }
+
+    /**
+     * Set the base path.
+     * @param baseDir The base path String.
+     */
+    public void setBaseDir(String baseDir)
+    {
+        manager.setBaseDir(baseDir);
     }
 
     /**
@@ -143,7 +166,7 @@ public class CatalogResolver implements EntityResolver
     public InputSource resolveEntity(String publicId, String systemId)
             throws SAXException
     {
-        String resolved = resolver.getResolvedEntity(publicId, systemId);
+        String resolved = getResolver().getResolvedEntity(publicId, systemId);
 
         if (resolved != null)
         {
@@ -199,4 +222,208 @@ public class CatalogResolver implements EntityResolver
         this.log = (log != null) ? log : new NoOpLog();
     }
 
+    private synchronized org.apache.xml.resolver.tools.CatalogResolver getResolver()
+    {
+        if (resolver == null)
+        {
+            resolver = new org.apache.xml.resolver.tools.CatalogResolver(manager);
+        }
+        return resolver;
+    }
+
+    /**
+     * Extend the CatalogManager to make the FileSystem and base directory accessible.
+     */
+    public static class CatalogManager extends org.apache.xml.resolver.CatalogManager
+    {
+        /** The FileSystem */
+        private FileSystem fs;
+
+        /** The base directory */
+        private String baseDir = System.getProperty("user.dir");
+
+        /**
+         * Set the FileSystem
+         * @param fileSystem The FileSystem in use.
+         */
+        public void setFileSystem(FileSystem fileSystem)
+        {
+            this.fs = fileSystem;
+        }
+
+        /**
+         * Retrieve the FileSystem.
+         * @return The FileSystem.
+         */
+        public FileSystem getFileSystem()
+        {
+            return this.fs;
+        }
+
+        /**
+         * Set the base directory.
+         * @param baseDir The base directory.
+         */
+        public void setBaseDir(String baseDir)
+        {
+            if (baseDir != null)
+            {
+                this.baseDir = baseDir;
+            }
+        }
+
+        /**
+         * Return the base directory.
+         * @return The base directory.
+         */
+        public String getBaseDir()
+        {
+            return this.baseDir;
+        }
+    }
+
+    /**
+     * Overrides the Catalog implementation to use the underlying FileSystem.
+     */
+    public static class Catalog extends org.apache.xml.resolver.Catalog
+    {
+        /** The FileSystem */
+        private FileSystem fs;
+
+        /** FileNameMap to determine the mime type */
+        private FileNameMap fileNameMap = URLConnection.getFileNameMap();
+
+        /**
+         * Load the catalogs.
+         * @throws IOException if an error occurs.
+         */
+        public void loadSystemCatalogs() throws IOException
+        {
+            fs = ((CatalogManager) catalogManager).getFileSystem();
+            String base = ((CatalogManager) catalogManager).getBaseDir();
+
+            Vector catalogs = catalogManager.getCatalogFiles();
+            if (catalogs != null)
+            {
+                for (int count = 0; count < catalogs.size(); count++)
+                {
+                    String fileName = (String) catalogs.elementAt(count);
+
+                    URL url = null;
+                    InputStream is = null;
+
+                    try
+                    {
+                        url = ConfigurationUtils.locate(fs, base, fileName);
+                        if (url != null)
+                        {
+                            is = fs.getInputStream(url);
+                        }
+                    }
+                    catch (ConfigurationException ce)
+                    {
+                        String name = (url == null) ? fileName : url.toString();
+                        // Ignore the exception.
+                        catalogManager.debug.message(DEBUG_ALL,
+                            "Unable to get input stream for " + name + ". " + ce.getMessage());
+                    }
+                    if (is != null)
+                    {
+                        String mimeType = fileNameMap.getContentTypeFor(fileName);
+                        try
+                        {
+                            if (mimeType != null)
+                            {
+                                parseCatalog(mimeType, is);
+                                continue;
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+                            // Ignore the exception.
+                            catalogManager.debug.message(DEBUG_ALL,
+                                "Exception caught parsing input stream for " + fileName + ". "
+                                + ex.getMessage());
+                        }
+                        finally
+                        {
+                            is.close();
+                        }
+                    }
+                    parseCatalog(base, fileName);
+                }
+            }
+
+        }
+
+        /**
+         * Parse the specified catalog file.
+         * @param baseDir The base directory, if not included in the file name.
+         * @param fileName The catalog file. May be a full URI String.
+         * @throws IOException If an error occurs.
+         */
+        public void parseCatalog(String baseDir, String fileName) throws IOException
+        {
+            base = ConfigurationUtils.locate(fs, baseDir, fileName);
+            catalogCwd = base;
+            default_override = catalogManager.getPreferPublic();
+            catalogManager.debug.message(DEBUG_NORMAL, "Parse catalog: " + fileName);
+
+            boolean parsed = false;
+
+            for (int count = 0; !parsed && count < readerArr.size(); count++)
+            {
+                CatalogReader reader = (CatalogReader) readerArr.get(count);
+                InputStream inStream;
+
+                try
+                {
+                    inStream = fs.getInputStream(base);
+                }
+                catch (Exception ex)
+                {
+                    catalogManager.debug.message(DEBUG_NORMAL, "Unable to access " + base
+                        + ex.getMessage());
+                    break;
+                }
+
+                try
+                {
+                    reader.readCatalog(this, inStream);
+                    parsed = true;
+                }
+                catch (CatalogException ce)
+                {
+                    catalogManager.debug.message(DEBUG_NORMAL, "Parse failed for " + fileName
+                            + ce.getMessage());
+                    if (ce.getExceptionType() == CatalogException.PARSE_FAILED)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        // try again!
+                        continue;
+                    }
+                }
+                finally
+                {
+                    try
+                    {
+                        inStream.close();
+                    }
+                    catch (IOException ioe)
+                    {
+                        // Ignore the exception.
+                        inStream = null;
+                    }
+                }
+            }
+
+            if (parsed)
+            {
+                parsePendingCatalogs();
+            }
+        }
+    }
 }
