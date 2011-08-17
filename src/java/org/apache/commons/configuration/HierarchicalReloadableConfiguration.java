c0d5a0c03814454491a9585e538a9c0f68746573
Oliver Heger <oheger@apache.org>
2011-08-17 19:56:38 +0000

    Javadoc improvements to make Checkstyle happy.
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/configuration/trunk@1158884 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/commons/configuration/HierarchicalReloadableConfiguration.java
+++ src/java/org/apache/commons/configuration/HierarchicalReloadableConfiguration.java
@@ -19,14 +19,23 @@ package org.apache.commons.configuration;
 import org.apache.commons.configuration.reloading.Reloadable;
 
 /**
+ * <p>A base class for hierarchical configurations with specific reloading
+ * requirements.</p>
+ * <p>This class manages a lock object which can be used for synchronization.</p>
  *
+ * @author <a
+ *         href="http://commons.apache.org/configuration/team-list.html">Commons
+ *         Configuration team</a>
+ * @version $Id: $
  */
 public class HierarchicalReloadableConfiguration extends HierarchicalConfiguration
     implements Reloadable
 {
-    private final Object reloadLock;
-
+    /** Constant for the name used for the lock object. */
     private static final String LOCK_NAME = "HierarchicalReloadableConfigurationLock";
+
+    /** The lock object used by this instance. */
+    private final Object reloadLock;
 
     /**
      * Creates a new instance of <code>HierarchicalReloadableConfiguration</code>.
@@ -37,6 +46,13 @@ public class HierarchicalReloadableConfiguration extends HierarchicalConfigurati
         reloadLock = new Lock(LOCK_NAME);
     }
 
+    /**
+     * Creates a new instance of
+     * <code>HierarchicalReloadableConfiguration</code> and initializes it with
+     * the given lock object.
+     *
+     * @param lock the lock object
+     */
     public HierarchicalReloadableConfiguration(Object lock)
     {
         super();
@@ -50,14 +66,12 @@ public class HierarchicalReloadableConfiguration extends HierarchicalConfigurati
      *
      * @param c the configuration that is to be copied (if <b>null</b>, this
      * constructor will behave like the standard constructor)
-     * @since 1.4
      */
     public HierarchicalReloadableConfiguration(HierarchicalConfiguration c)
     {
         super(c);
         reloadLock = new Lock(LOCK_NAME);
     }
-
 
     public Object getReloadLock()
     {
