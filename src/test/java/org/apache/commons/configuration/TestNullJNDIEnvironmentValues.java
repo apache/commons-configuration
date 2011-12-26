3c5dd6325184084168d1b60ba8c94bdfb18d16d2
Oliver Heger <oheger@apache.org>
2011-12-26 21:18:18 +0000

    Converted tests to JUnit 4, fixed warnings.
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/configuration/trunk@1224816 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/java/org/apache/commons/configuration/TestNullJNDIEnvironmentValues.java
+++ src/test/java/org/apache/commons/configuration/TestNullJNDIEnvironmentValues.java
@@ -17,33 +17,44 @@
 
 package org.apache.commons.configuration;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
 import java.util.Iterator;
 
-import junit.framework.TestCase;
+import org.junit.Before;
+import org.junit.Test;
 
-public class TestNullJNDIEnvironmentValues extends TestCase
+public class TestNullJNDIEnvironmentValues
 {
     private JNDIConfiguration conf = null;
 
+    @Before
     public void setUp() throws Exception
     {
         System.setProperty("java.naming.factory.initial", TestJNDIConfiguration.CONTEXT_FACTORY);
-        
+
         conf = new JNDIConfiguration();
         conf.setThrowExceptionOnMissing(false);
     }
 
+    @Test
     public void testThrowExceptionOnMissing()
     {
         assertFalse("Throw Exception Property is set!", conf.isThrowExceptionOnMissing());
     }
 
+    @Test
     public void testSimpleGet() throws Exception
     {
         String s = conf.getString("test.key");
         assertEquals("jndivalue", s);
     }
 
+    @Test
     public void testMoreGets() throws Exception
     {
         String s = conf.getString("test.key");
@@ -52,39 +63,45 @@ public class TestNullJNDIEnvironmentValues extends TestCase
         assertEquals(1, conf.getShort("test.short"));
     }
 
+    @Test
     public void testGetMissingKey() throws Exception
     {
         assertNull("Missing Key is not null!", conf.getString("test.imaginarykey"));
     }
 
+    @Test
     public void testGetMissingKeyWithDefault() throws Exception
     {
         String result = conf.getString("test.imaginarykey", "bob");
         assertEquals("bob", result);
     }
 
+    @Test
     public void testContainsKey() throws Exception
     {
         assertTrue(conf.containsKey("test.key"));
         assertTrue(!conf.containsKey("test.imaginarykey"));
     }
-    
+
+    @Test
     public void testClearProperty()
     {
         assertNotNull("null short for the 'test.short' key", conf.getShort("test.short", null));
         conf.clearProperty("test.short");
         assertNull("'test.short' property not cleared", conf.getShort("test.short", null));
     }
-    
+
+    @Test
     public void testIsEmpty()
     {
         assertFalse("the configuration shouldn't be empty", conf.isEmpty());
     }
-    
+
+    @Test
     public void testGetKeys() throws Exception
     {
         boolean found = false;
-        Iterator it = conf.getKeys();
+        Iterator<String> it = conf.getKeys();
 
         assertTrue("no key found", it.hasNext());
 
@@ -96,17 +113,19 @@ public class TestNullJNDIEnvironmentValues extends TestCase
         assertTrue("'test.boolean' key not found", found);
     }
 
+    @Test
     public void testGetKeysWithUnknownPrefix()
     {
         // test for a unknown prefix
-        Iterator it = conf.getKeys("foo.bar");
+        Iterator<String> it = conf.getKeys("foo.bar");
         assertFalse("no key should be found", it.hasNext());
     }
 
+    @Test
     public void testGetKeysWithExistingPrefix()
     {
         // test for an existing prefix
-        Iterator it = conf.getKeys("test");
+        Iterator<String> it = conf.getKeys("test");
         boolean found = false;
         while (it.hasNext() && !found)
         {
@@ -116,10 +135,11 @@ public class TestNullJNDIEnvironmentValues extends TestCase
         assertTrue("'test.boolean' key not found", found);
     }
 
+    @Test
     public void testGetKeysWithKeyAsPrefix()
     {
         // test for a prefix matching exactly the key of a property
-        Iterator it = conf.getKeys("test.boolean");
+        Iterator<String> it = conf.getKeys("test.boolean");
         boolean found = false;
         while (it.hasNext() && !found)
         {
@@ -128,5 +148,4 @@ public class TestNullJNDIEnvironmentValues extends TestCase
 
         assertTrue("'test.boolean' key not found", found);
     }
-
 }
