95728ed600b8d4671d68b280b6d590d5d6386e41
Oliver Heger <oheger@apache.org>
2013-03-19 21:09:08 +0000

    Added a new parameters interface for properties of a properties configuration.
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/configuration/trunk@1458518 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/main/java/org/apache/commons/configuration/builder/PropertiesBuilderProperties.java
@@ -0,0 +1,69 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.commons.configuration.builder;
+
+import org.apache.commons.configuration.PropertiesConfiguration.IOFactory;
+import org.apache.commons.configuration.PropertiesConfigurationLayout;
+
+/**
+ * <p>
+ * Definition of a parameters interface for properties configurations.
+ * </p>
+ * <p>
+ * This interface defines additional properties which can be set when
+ * initializing a {@code PropertiesConfiguration} object.
+ * </p>
+ *
+ * @version $Id$
+ * @since 2.0
+ * @param <T> the type of the result of all set methods for method chaining
+ */
+public interface PropertiesBuilderProperties<T>
+{
+    /**
+     * Sets a flag whether include files are supported by the properties
+     * configuration object. If set to <b>true</b>, files listed by an include
+     * property are loaded automatically.
+     *
+     * @param f the value of the flag
+     * @return a reference to this object for method chaining
+     */
+    T setIncludesAllowed(boolean f);
+
+    /**
+     * Sets the layout object for the properties configuration object. With this
+     * method a custom layout object can be set. If no layout is provided, the
+     * configuration will use a default layout.
+     *
+     * @param layout the {@code PropertiesConfigurationLayout} object to be used
+     *        by the configuration
+     * @return a reference to this object for method chaining
+     */
+    T setLayout(PropertiesConfigurationLayout layout);
+
+    /**
+     * Sets the {@code IOFactory} to be used by the properties configuration
+     * object. With this method a custom factory for input and output streams
+     * can be set. This allows customizing the format of properties read or
+     * written by the configuration. If no {@code IOFactory} is provided, the
+     * configuration uses a default one.
+     *
+     * @param factory the {@code IOFactory} to be used by the configuration
+     * @return a reference to this object for method chaining
+     */
+    T setIOFactory(IOFactory factory);
+}
