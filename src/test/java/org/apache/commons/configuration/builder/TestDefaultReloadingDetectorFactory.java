d0dc0246cea1705c91f0522149fd9eb10d20b0f7
Oliver Heger <oheger@apache.org>
2013-03-26 15:11:15 +0000

    Added a default implementation of ReloadingDetectorFactory which creates FileHandlerReloadingDetector objects.
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/configuration/trunk@1461171 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/test/java/org/apache/commons/configuration/builder/TestDefaultReloadingDetectorFactory.java
@@ -0,0 +1,62 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.commons.configuration.builder;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertSame;
+
+import org.apache.commons.configuration.ConfigurationException;
+import org.apache.commons.configuration.io.FileHandler;
+import org.apache.commons.configuration.reloading.FileHandlerReloadingDetector;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Test class for {@code DefaultReloadingDetectorFactory}.
+ *
+ * @version $Id: $
+ */
+public class TestDefaultReloadingDetectorFactory
+{
+    /** The factory to be tested. */
+    private DefaultReloadingDetectorFactory factory;
+
+    @Before
+    public void setUp() throws Exception
+    {
+        factory = new DefaultReloadingDetectorFactory();
+    }
+
+    /**
+     * Tests whether a reloading detector is created correctly.
+     */
+    @Test
+    public void testCreateReloadingDetector() throws ConfigurationException
+    {
+        FileHandler handler = new FileHandler();
+        FileBasedBuilderParametersImpl params =
+                new FileBasedBuilderParametersImpl();
+        final long refreshDelay = 10000L;
+        params.setReloadingRefreshDelay(refreshDelay);
+        FileHandlerReloadingDetector detector =
+                (FileHandlerReloadingDetector) factory.createReloadingDetector(
+                        handler, params);
+        assertSame("Wrong file handler", handler, detector.getFileHandler());
+        assertEquals("Wrong refresh delay", refreshDelay,
+                detector.getRefreshDelay());
+    }
+}
