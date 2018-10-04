0b7369cbb67e46586e9e031c693564ce7ca583c7
Gary D. Gregory <ggregory@apache.org>
2018-10-04 20:06:34 +0000

    [CONFIGURATION-726] Add support for Commons Text 1.5 new string lookups as default lookups.
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/configuration/trunk@1842848 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/commons/configuration2/interpol/DefaultLookups.java
+++ src/main/java/org/apache/commons/configuration2/interpol/DefaultLookups.java
@@ -20,63 +20,163 @@ import org.apache.commons.text.lookup.StringLookupFactory;
 
 /**
  * <p>
- * An enumeration class defining constants for the {@code Lookup} objects
- * available for each {@code Configuration} object per default.
+ * An enumeration class defining constants for the {@code Lookup} objects available for each {@code Configuration}
+ * object per default.
  * </p>
  * <p>
- * When a new configuration object derived from {@code AbstractConfiguration} is
- * created it installs a {@link ConfigurationInterpolator} with a default set of
- * {@link Lookup} objects. These lookups are defined by this enumeration class.
+ * When a new configuration object derived from {@code AbstractConfiguration} is created it installs a
+ * {@link ConfigurationInterpolator} with a default set of {@link Lookup} objects. These lookups are defined by this
+ * enumeration class.
  * </p>
  * <p>
- * All the default {@code Lookup} classes are state-less, thus their instances
- * can be shared between multiple configuration objects. Therefore, it makes
- * sense to keep shared instances in this enumeration class.
+ * All the default {@code Lookup} classes are state-less, thus their instances can be shared between multiple
+ * configuration objects. Therefore, it makes sense to keep shared instances in this enumeration class.
  * </p>
- *
+ * 
+ * Provides access to lookups defined in Apache Commons Text:
+ * <ul>
+ * <li>"base64" for the {@code Base64StringLookup} since Apache Commons Text 1.5.</li>
+ * <li>"const" for the {@code ConstantStringLookup} since Apache Commons Text 1.5.</li>
+ * <li>"date" for the {@code DateStringLookup}.</li>
+ * <li>"env" for the {@code EnvironmentVariableStringLookup}.</li>
+ * <li>"file" for the {@code FileStringLookup} since Apache Commons Text 1.5.</li>
+ * <li>"java" for the {@code JavaPlatformStringLookup}.</li>
+ * <li>"localhost" for the {@code LocalHostStringLookup}, see {@code #localHostStringLookup()} for key names; since
+ * Apache Commons Text 1.3.</li>
+ * <li>"properties" for the {@code PropertiesStringLookup} since Apache Commons Text 1.5.</li>
+ * <li>"resourceBundle" for the {@code ResourceBundleStringLookup} since Apache Commons Text 1.5.</li>
+ * <li>"script" for the {@code ScriptStringLookup} since Apache Commons Text 1.5.</li>
+ * <li>"sys" for the {@code SystemPropertyStringLookup}.</li>
+ * <li>"url" for the {@code UrlStringLookup} since Apache Commons Text 1.5.</li>
+// * <li>"urlDecode" for the {@code UrlDecoderStringLookup} since Apache Commons Text 1.6.</li>
+// * <li>"urlEncode" for the {@code UrlEncoderStringLookup} since Apache Commons Text 1.6.</li>
+ * <li>"xml" for the {@code XmlStringLookup} since Apache Commons Text 1.5.</li>
+ * </ul>
+ * 
  * @version $Id$
  * @since 2.0
  */
-public enum DefaultLookups
-{
-    /** @since 2.4 */
+public enum DefaultLookups {
+
+    /**
+     * The lookup for Base64.
+     * 
+     * @since 2.4
+     */
+    BASE64("base64", new StringLookupAdapter(StringLookupFactory.INSTANCE.base64StringLookup())),
+
+    /**
+     * The lookup for constants.
+     * 
+     * @since 2.4
+     */
+    CONST("const", new StringLookupAdapter(StringLookupFactory.INSTANCE.constantStringLookup())),
+
+    /**
+     * The lookup for dates.
+     * 
+     * @since 2.4
+     */
+    DATE("date", new StringLookupAdapter(StringLookupFactory.INSTANCE.dateStringLookup())),
+
+    /**
+     * The lookup for environment properties.
+     */
+    ENVIRONMENT("env", new StringLookupAdapter(StringLookupFactory.INSTANCE.environmentVariableStringLookup())),
+
+    /**
+     * The lookup for files.
+     * 
+     * @since 2.4
+     */
+    FILE("file", new StringLookupAdapter(StringLookupFactory.INSTANCE.fileStringLookup())),
+
+    /**
+     * The lookup for Java platform information.
+     * 
+     * @since 2.4
+     */
+    JAVA("java", new StringLookupAdapter(StringLookupFactory.INSTANCE.javaPlatformStringLookup())),
+
+    /**
+     * The lookup for localhost information.
+     * 
+     * @since 2.4
+     */
     LOCAL_HOST("localhost", new StringLookupAdapter(StringLookupFactory.INSTANCE.localHostStringLookup())),
-    
-    /** The lookup for system properties. */
-    SYSTEM_PROPERTIES("sys", new SystemPropertiesLookup()),
 
-    /** The lookup for environment properties. */
-    ENVIRONMENT("env", new EnvironmentLookup()),
+    /**
+     * The lookup for properties.
+     * 
+     * @since 2.4
+     */
+    PROPERTIES("properties", new StringLookupAdapter(StringLookupFactory.INSTANCE.propertiesStringLookup())),
 
-    /** The lookup for constants. */
-    CONST("const", new ConstantLookup());
+    /**
+     * The lookup for resource bundles.
+     * 
+     * @since 2.4
+     */
+    RESOURCE_BUNDLE("resourceBundle",
+            new StringLookupAdapter(StringLookupFactory.INSTANCE.resourceBundleStringLookup())),
 
-    /** The prefix under which the associated lookup object is registered. */
-    private final String prefix;
+    /**
+     * The lookup for scripts.
+     * 
+     * @since 2.4
+     */
+    SCRIPT("script", new StringLookupAdapter(StringLookupFactory.INSTANCE.scriptStringLookup())),
+
+    /**
+     * The lookup for system properties.
+     */
+    SYSTEM_PROPERTIES("sys", new StringLookupAdapter(StringLookupFactory.INSTANCE.systemPropertyStringLookup())),
+
+    /**
+     * The lookup for URLs.
+     * 
+     * @since 2.4
+     */
+    URL("url", new StringLookupAdapter(StringLookupFactory.INSTANCE.urlStringLookup())),
+
+    /**
+     * The lookup for URL decoding.
+     * 
+     * @since 2.4
+     */
+    // URL_DECODE("urlDecode", new StringLookupAdapter(StringLookupFactory.INSTANCE.urlDecodeStringLookup())),
+
+    /**
+     * The lookup for URL decoding.
+     * 
+     * @since 2.4
+     */
+    // URL_ENCODE("urlEncode", new StringLookupAdapter(StringLookupFactory.INSTANCE.urlEncodeStringLookup())),
+
+    /**
+     * The lookup for URL decoding.
+     * 
+     * @since 2.4
+     */
+    XML("xml", new StringLookupAdapter(StringLookupFactory.INSTANCE.xmlStringLookup()));
 
     /** The associated lookup instance. */
     private final Lookup lookup;
 
-    /**
-     * Creates a new instance of {@code DefaultLookups} and sets the prefix and
-     * the associated lookup instance.
-     *
-     * @param prefix the prefix
-     * @param lookup the {@code Lookup} instance
-     */
-    private DefaultLookups(final String prefix, final Lookup lookup)
-    {
-        this.prefix = prefix;
-        this.lookup = lookup;
-    }
+    /** The prefix under which the associated lookup object is registered. */
+    private final String prefix;
 
     /**
-     * Returns the standard prefix for the lookup object of this kind.
+     * Creates a new instance of {@code DefaultLookups} and sets the prefix and the associated lookup instance.
      *
-     * @return the prefix
+     * @param prefix
+     *            the prefix
+     * @param lookup
+     *            the {@code Lookup} instance
      */
-    public String getPrefix() {
-        return prefix;
+    private DefaultLookups(final String prefix, final Lookup lookup) {
+        this.prefix = prefix;
+        this.lookup = lookup;
     }
 
     /**
@@ -86,5 +186,14 @@ public enum DefaultLookups
      */
     public Lookup getLookup() {
         return lookup;
+    }
+
+    /**
+     * Returns the standard prefix for the lookup object of this kind.
+     *
+     * @return the prefix
+     */
+    public String getPrefix() {
+        return prefix;
     }
 }
