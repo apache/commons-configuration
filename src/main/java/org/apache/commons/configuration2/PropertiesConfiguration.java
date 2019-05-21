76abecc4ac6a6e89d58d5809247df8f3d4e03bcf
Gary Gregory <garydgregory@users.noreply.github.com>
2019-05-21 17:45:18 -0400

    [CONFIGURATION-745] Add the special key "includeoptional" for properties files. (#27)
    * [CONFIGURATION-745] Add the special key "includeoptional" for properties
    files.

--- src/main/java/org/apache/commons/configuration2/PropertiesConfiguration.java
+++ src/main/java/org/apache/commons/configuration2/PropertiesConfiguration.java
@@ -243,6 +243,15 @@ public class PropertiesConfiguration extends BaseConfiguration
      */
     private static String include = "include";
 
+    /**
+     * This is the name of the property that can point to other
+     * properties file for including other properties files.
+     * <p>
+     * If the file is absent, processing continues normally.
+     * </p>
+     */
+    private static String includeOptional = "includeoptional";
+
     /** The list of possible key/value separators */
     private static final char[] SEPARATORS = new char[] {'=', ':'};
 
@@ -292,6 +301,21 @@ public class PropertiesConfiguration extends BaseConfiguration
     }
 
     /**
+     * Gets the property value for including other properties files.
+     * By default it is "include".
+     * <p>
+     * If the file is absent, processing continues normally.
+     * </p>
+     *
+     * @return A String.
+     * @since 2.5
+     */
+    public static String getIncludeOptional()
+    {
+        return PropertiesConfiguration.includeOptional;
+    }
+
+    /**
      * Sets the property value for including other properties files.
      * By default it is "include".
      *
@@ -300,6 +324,21 @@ public class PropertiesConfiguration extends BaseConfiguration
     public static void setInclude(final String inc)
     {
         PropertiesConfiguration.include = inc;
+    }
+
+    /**
+     * Sets the property value for including other properties files.
+     * By default it is "include".
+     * <p>
+     * If the file is absent, processing continues normally.
+     * </p>
+     *
+     * @param inc A String.
+     * @since 2.5
+     */
+    public static void setIncludeOptional(final String inc)
+    {
+        PropertiesConfiguration.includeOptional = inc;
     }
 
     /**
@@ -586,7 +625,22 @@ public class PropertiesConfiguration extends BaseConfiguration
                         getListDelimiterHandler().split(value, true);
                 for (final String f : files)
                 {
-                    loadIncludeFile(interpolate(f));
+                    loadIncludeFile(interpolate(f), false);
+                }
+            }
+            result = false;
+        }
+
+        else if (StringUtils.isNotEmpty(getIncludeOptional())
+            && key.equalsIgnoreCase(getIncludeOptional()))
+        {
+            if (isIncludesAllowed())
+            {
+                final Collection<String> files =
+                        getListDelimiterHandler().split(value, true);
+                for (final String f : files)
+                {
+                    loadIncludeFile(interpolate(f), true);
                 }
             }
             result = false;
@@ -1538,7 +1592,7 @@ public class PropertiesConfiguration extends BaseConfiguration
             initialMap.put("\f", "\\f");
             initialMap.put("\r", "\\r");
             JUP_CHARS_ESCAPE = Collections.unmodifiableMap(initialMap);
-        };
+        }
 
         /**
          * Creates a new instance of {@code JupPropertiesWriter}.
@@ -1740,9 +1794,10 @@ public class PropertiesConfiguration extends BaseConfiguration
      * this properties file is tried.
      *
      * @param fileName the name of the file to load
+     * @param optional whether or not the {@code fileName} is optional
      * @throws ConfigurationException if loading fails
      */
-    private void loadIncludeFile(final String fileName) throws ConfigurationException
+    private void loadIncludeFile(final String fileName, boolean optional) throws ConfigurationException
     {
         if (locator == null)
         {
@@ -1759,6 +1814,11 @@ public class PropertiesConfiguration extends BaseConfiguration
             {
                 url = locateIncludeFile(baseURL.toString(), fileName);
             }
+        }
+
+        if (optional && url == null)
+        {
+            return;
         }
 
         if (url == null)
