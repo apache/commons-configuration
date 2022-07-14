86d695f7fe59504ba64398b6de3faf10786076f3
Gary Gregory <garydgregory@gmail.com>
2022-07-14 09:47:37 -0400

    Sort members.

--- src/main/java/org/apache/commons/configuration2/convert/AbstractListDelimiterHandler.java
+++ src/main/java/org/apache/commons/configuration2/convert/AbstractListDelimiterHandler.java
@@ -37,33 +37,6 @@ import java.util.LinkedList;
  */
 public abstract class AbstractListDelimiterHandler implements ListDelimiterHandler {
     /**
-     * {@inheritDoc} Depending on the type of the passed in object the following things happen:
-     * <ul>
-     * <li>Strings are checked for delimiter characters and split if necessary. This is done by calling the {@code split()}
-     * method.</li>
-     * <li>For objects implementing the {@code Iterable} interface, the corresponding {@code Iterator} is obtained, and
-     * contained elements are added to the resulting iteration.</li>
-     * <li>Arrays are treated as {@code Iterable} objects.</li>
-     * <li>All other types are directly inserted.</li>
-     * <li>Recursive combinations are supported, e.g. a collection containing an array that contains strings: The resulting
-     * collection will only contain primitive objects.</li>
-     * </ul>
-     */
-    @Override
-    public Iterable<?> parse(final Object value) {
-        return flatten(value);
-    }
-
-    /**
-     * {@inheritDoc} This implementation handles the case that the passed in string is <b>null</b>. In this case, an empty
-     * collection is returned. Otherwise, this method delegates to {@link #splitString(String, boolean)}.
-     */
-    @Override
-    public Collection<String> split(final String s, final boolean trim) {
-        return s == null ? new ArrayList<>(0) : splitString(s, trim);
-    }
-
-    /**
      * {@inheritDoc} This implementation checks whether the object to be escaped is a string. If yes, it delegates to
      * {@link #escapeString(String)}, otherwise no escaping is performed. Eventually, the passed in transformer is invoked
      * so that additional encoding can be performed.
@@ -74,16 +47,6 @@ public abstract class AbstractListDelimiterHandler implements ListDelimiterHandl
     }
 
     /**
-     * Actually splits the passed in string which is guaranteed to be not <b>null</b>. This method is called by the base
-     * implementation of the {@code split()} method. Here the actual splitting logic has to be implemented.
-     *
-     * @param s the string to be split (not <b>null</b>)
-     * @param trim a flag whether the single components have to be trimmed
-     * @return a collection with the extracted components of the passed in string
-     */
-    protected abstract Collection<String> splitString(String s, boolean trim);
-
-    /**
      * Escapes the specified string. This method is called by {@code escape()} if the passed in object is a string. Concrete
      * subclasses have to implement their specific escaping logic here, so that the list delimiters they support are
      * properly escaped.
@@ -92,6 +55,17 @@ public abstract class AbstractListDelimiterHandler implements ListDelimiterHandl
      * @return the escaped string
      */
     protected abstract String escapeString(String s);
+
+    /**
+     * Performs the actual work as advertised by the {@code parse()} method. This method delegates to
+     * {@link #flatten(Object, int)} without specifying a limit.
+     *
+     * @param value the value to be processed
+     * @return a &quot;flat&quot; collection containing all primitive values of the passed in object
+     */
+    private Collection<?> flatten(final Object value) {
+        return flatten(value, Integer.MAX_VALUE);
+    }
 
     /**
      * Extracts all values contained in the specified object up to the given limit. The passed in object is evaluated (if
@@ -126,17 +100,6 @@ public abstract class AbstractListDelimiterHandler implements ListDelimiterHandl
     }
 
     /**
-     * Performs the actual work as advertised by the {@code parse()} method. This method delegates to
-     * {@link #flatten(Object, int)} without specifying a limit.
-     *
-     * @param value the value to be processed
-     * @return a &quot;flat&quot; collection containing all primitive values of the passed in object
-     */
-    private Collection<?> flatten(final Object value) {
-        return flatten(value, Integer.MAX_VALUE);
-    }
-
-    /**
      * Flattens the given iterator. For each element in the iteration {@code flatten()} is called recursively.
      *
      * @param target the target collection
@@ -150,4 +113,41 @@ public abstract class AbstractListDelimiterHandler implements ListDelimiterHandl
             size = target.size();
         }
     }
+
+    /**
+     * {@inheritDoc} Depending on the type of the passed in object the following things happen:
+     * <ul>
+     * <li>Strings are checked for delimiter characters and split if necessary. This is done by calling the {@code split()}
+     * method.</li>
+     * <li>For objects implementing the {@code Iterable} interface, the corresponding {@code Iterator} is obtained, and
+     * contained elements are added to the resulting iteration.</li>
+     * <li>Arrays are treated as {@code Iterable} objects.</li>
+     * <li>All other types are directly inserted.</li>
+     * <li>Recursive combinations are supported, e.g. a collection containing an array that contains strings: The resulting
+     * collection will only contain primitive objects.</li>
+     * </ul>
+     */
+    @Override
+    public Iterable<?> parse(final Object value) {
+        return flatten(value);
+    }
+
+    /**
+     * {@inheritDoc} This implementation handles the case that the passed in string is <b>null</b>. In this case, an empty
+     * collection is returned. Otherwise, this method delegates to {@link #splitString(String, boolean)}.
+     */
+    @Override
+    public Collection<String> split(final String s, final boolean trim) {
+        return s == null ? new ArrayList<>(0) : splitString(s, trim);
+    }
+
+    /**
+     * Actually splits the passed in string which is guaranteed to be not <b>null</b>. This method is called by the base
+     * implementation of the {@code split()} method. Here the actual splitting logic has to be implemented.
+     *
+     * @param s the string to be split (not <b>null</b>)
+     * @param trim a flag whether the single components have to be trimmed
+     * @return a collection with the extracted components of the passed in string
+     */
+    protected abstract Collection<String> splitString(String s, boolean trim);
 }
