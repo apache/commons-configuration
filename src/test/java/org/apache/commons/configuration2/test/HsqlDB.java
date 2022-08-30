a702b787138830a9f5c644207908a199dd8d210d
Gary Gregory <garydgregory@gmail.com>
2022-08-30 10:46:49 -0400

    Move Apache license header to the top of the file

--- src/test/java/org/apache/commons/configuration2/test/HsqlDB.java
+++ src/test/java/org/apache/commons/configuration2/test/HsqlDB.java
@@ -1,5 +1,3 @@
-package org.apache.commons.configuration2.test;
-
 /*
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
@@ -16,6 +14,8 @@ package org.apache.commons.configuration2.test;
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+package org.apache.commons.configuration2.test;
 
 import java.io.FileReader;
 import java.sql.Connection;
