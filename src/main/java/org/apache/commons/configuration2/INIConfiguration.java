1e0744debb7ae39b80b4b702dce3a233a15d02bd
Thomas Steiner <thomas.steiner@engel.at>
2024-04-23 15:50:03 +0200

    Remove trailing whitespaces

--- src/main/java/org/apache/commons/configuration2/INIConfiguration.java
+++ src/main/java/org/apache/commons/configuration2/INIConfiguration.java
@@ -134,7 +134,7 @@ import org.apache.commons.configuration2.tree.TrackedNodeModel;
  *  [sectionSeparators]
  *  passwd : abc=def
  *  a:b = "value"
- * 
+ *
  *  []
  *  var = emptySection
  * </pre>
@@ -158,7 +158,7 @@ import org.apache.commons.configuration2.tree.TrackedNodeModel;
  * {@code abc=def}. This default behavior can be changed by using quotes. If there is a separator character before the
  * first quote character (ignoring whitespace), this character is used as separator. Thus the second property definition
  * in the section has the key {@code a:b} and the value {@code value}.</li>
- * <li>The empty section is added using a key consisting of a single space character. The parameters can be accessed 
+ * <li>The empty section is added using a key consisting of a single space character. The parameters can be accessed
  * using {@code getProperty(" .var")}</li>
  * </ul>
  * <p>
