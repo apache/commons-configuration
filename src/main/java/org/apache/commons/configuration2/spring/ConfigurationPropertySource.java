ab388c3a0c97cd249dfc2945b149da8b34e01da6
Andrea Bollini <andrea.bollini@4science.com>
2024-05-27 11:02:22 +0200

    CONFIGURATION-846 fix wrong behavior with unset property reported as empty array

--- src/main/java/org/apache/commons/configuration2/spring/ConfigurationPropertySource.java
+++ src/main/java/org/apache/commons/configuration2/spring/ConfigurationPropertySource.java
@@ -40,7 +40,13 @@ public class ConfigurationPropertySource extends EnumerablePropertySource<Config
     @Override
     public Object getProperty(final String name) {
         final String[] propValue = source.getStringArray(name);
-        return propValue != null && propValue.length == 1 ? propValue[0] : propValue;
+        if (propValue == null || propValue.length == 0) {
+            return null;
+        } else if (propValue.length == 1) {
+            return propValue[0];
+        } else {
+            return propValue;
+        }
     }
 
     @Override
