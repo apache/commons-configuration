99c7f7d8ed43bee7d7a13649f2c73f9b10903e0c
Gary Gregory <garydgregory@gmail.com>
2024-05-30 07:45:12 -0400

    Add missing @Override

--- src/test/java/org/apache/commons/configuration2/web/TestServletRequestConfiguration.java
+++ src/test/java/org/apache/commons/configuration2/web/TestServletRequestConfiguration.java
@@ -94,6 +94,7 @@ public class TestServletRequestConfiguration extends TestAbstractConfiguration {
         assertThrows(UnsupportedOperationException.class, super::testClearProperty);
     }
 
+    @Override
     @Test
     public void testContainsValue() {
         assertFalse(getConfiguration().containsValue(null));
