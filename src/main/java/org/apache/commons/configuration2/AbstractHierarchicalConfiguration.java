92c9c8e7ef12c56d00a531162e18d95cc84461fa
Gary Gregory <garydgregory@gmail.com>
2025-12-31 18:13:50 -0500

    Javadoc: Empty Javadoc line before the 1st tag.

--- src/main/java/org/apache/commons/configuration2/AbstractHierarchicalConfiguration.java
+++ src/main/java/org/apache/commons/configuration2/AbstractHierarchicalConfiguration.java
@@ -449,6 +449,7 @@ public abstract class AbstractHierarchicalConfiguration<T> extends AbstractConfi
     /**
      * Tests whether this configuration contains one or more matches to this value. This operation stops at first
      * match but may be more expensive than the containsKey method.
+     *
      * @since 2.11.0
      */
     @Override
